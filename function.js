e5fc0410ed84acb86c29a340a93d0b0770dff596
Farhan Alam <108146290+farhandotdev@users.noreply.github.com>
2022-08-04 00:29:25 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,12 @@
+const add = (a,b) => a+b;
+console.log(`Addition is ${add(4,2)}`);
+
+const sub = (a,b) => a-b;
+console.log(`Substraction is ${sub(8,3)}`)
+
+const mult = (a,b) => a*b;
+console.log(`Multiplication is ${mult(8,9)}`);
+
+const div = (a,b) => a/b;
+console.log(`Division is ${div(3,2)}`)
+ 
